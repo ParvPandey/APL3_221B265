e950b3a0090ed5db09148bc8db5d47b9c59c0a3b
221B265 <221B265@juet.co.in>
2025-03-28 14:22:25 +0530

    LAB-EXERCISE : 5

---
+++ LE5/Beverage.java
@@ -0,0 +1,16 @@
+abstract class Beverage{
+    void pour(int qty){
+        System.out.println("Add "+qty+" ml of beverage");
+    }
+    abstract void addCondiment();
+    protected void stir(){}
+    private void serve(){
+        System.out.println("Serve through waiter");
+    }
+    public final void templateMethod(int qty){
+        pour(qty);
+        addCondiment();
+        stir();
+        serve();
+    }
+}
---
+++ LE5/Whiskey.java
@@ -0,0 +1,8 @@
+class Whiskey extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add Ice");
+    }
+    protected void stir(){
+        System.out.println("Stir for 27 seconds");
+    }
+}
