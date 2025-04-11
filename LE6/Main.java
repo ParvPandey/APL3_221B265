98ddc8d72e98ea3a24b43f77a4c35ab855b45a72
221B265 <221B265@juet.co.in>
2025-04-11 14:24:29 +0530

    LAB-EXERCISE : 6

---
+++ LE6/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String args[])
+    {
+        Offering offering=new Coffee();
+        offering=new Rum(offering);
+        offering=new Rum(offering);
+        System.out.println(offering.getName()+" "+offering.getPrice());
+    }
+}
